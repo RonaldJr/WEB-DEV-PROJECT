ae34dfaff262035010d496fcbb0191f92251bf78
buritooo <mackithepanget@gmail.com>
2024-01-24 12:44:18 +0800

    Update App.js

--- skillswapappreact/src/App.js
+++ skillswapappreact/src/App.js
@@ -1,11 +1,15 @@
 // App.js
-import React, { useState } from 'react';
-import LoginForm from './LoginForm';
-import HomePage from './HomePage';
-import './App.css';
+import React, { useState } from "react";
+import LoginForm from "./LoginForm";
+import HomePage from "./HomePage";
+import AboutUs from "./AboutUs"; // Import the new component
+import Member from "./Member"; // Import the new component
+import "./App.css";
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [showAboutUs, setShowAboutUs] = useState(false);
+  const [showMember, setShowMember] = useState(false);
 
   const handleLogin = (username, password) => {
     // Implement your login logic here
@@ -17,13 +21,42 @@ const App = () => {
     setLoggedIn(false);
   };
 
+  const handleAboutUsClick = () => {
+    setShowAboutUs(true);
+  };
+
+  const handleAboutUsClose = () => {
+    setShowAboutUs(false);
+  };
+
+  const handleMemberClick = () => {
+    setShowMember(true);
+  };
+
+  const handleMemberClose = () => {
+    setShowMember(false);
+  };
+
   return (
     <div className="App">
       <header className="header">
         <h1>Welcome to SwapSkill</h1>
+        <ul className="nav-links">
+          <li>
+            <button onClick={handleAboutUsClick}>About Us!</button>
+          </li>
+          <li>
+            <button onClick={handleMemberClick}>Meet our Developers!</button>
+          </li>
+        </ul>
       </header>
+
       <div className="content">
-        {loggedIn ? (
+        {showAboutUs ? (
+          <AboutUs handleAboutUsClick={handleAboutUsClose} />
+        ) : showMember ? (
+          <Member handleMemberClick={handleMemberClose} />
+        ) : loggedIn ? (
           <HomePage handleLogout={handleLogout} loggedIn={loggedIn} />
         ) : (
           <div className="login-form-container">
@@ -34,5 +67,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
