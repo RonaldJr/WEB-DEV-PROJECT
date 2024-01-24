41ae226d097209cf3c4115bd340b1f373062efc5
buritooo <mackithepanget@gmail.com>
2024-01-24 13:11:01 +0800

    adding memberjs

---
+++ Member.js
@@ -0,0 +1,36 @@
+// Member.js
+import React from "react";
+import "./Member.css"; // Import the CSS file for Member
+
+const Member = ({ handleMemberClick }) => {
+  return (
+    <div className="Member">
+      <div className="member-container">
+        <div className="member-item">
+          <img src={require("./members/rupert.jpg")} alt="Rupert" />
+          <p>Ruperto Marqueta</p>
+        </div>
+
+        <div className="member-item">
+          <img src={require("./members/angelo.jpg")} alt="Angelo" />
+          <p>Angelo Padilla</p>
+        </div>
+
+        <div className="member-item">
+          <img src={require("./members/ronald.jpg")} alt="Ronald" />
+          <p>RonaldJr. E. Labrado</p>
+        </div>
+
+        <div className="member-item">
+          <img src={require("./members/macki.jpg")} alt="MACKI" />
+          <p>Macki Tabancay</p>
+        </div>
+      </div>
+      <button className="closeButton" onClick={handleMemberClick}>
+        Close
+      </button>
+    </div>
+  );
+};
+
+export default Member;
