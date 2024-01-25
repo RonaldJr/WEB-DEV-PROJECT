6858be22d5e442f285c9e12cd783397d32f767da
RonaldJr <132431732+RonaldJr@users.noreply.github.com>
2024-01-25 10:43:32 +0800

    ada

--- skillswapappreact/src/App.js
+++ skillswapappreact/src/App.js
@@ -43,7 +43,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="header">
-      <img src={require('./members/login.jpg')} alt="logo" />
+      <img src={require('./members/tupp.png')} alt="logo" />
         <h1>Welcome to SwapSkill</h1>
         <ul className="nav-links">
           <li>
